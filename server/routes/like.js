2488b5ede6259f010a3fe1f6a32b62e27f1ea9a1
Dhanusha R <162958967+dhanusha013@users.noreply.github.com>
2025-07-14 20:48:58 +0530

    Update like.js

--- server/routes/like.js
+++ server/routes/like.js
@@ -2,6 +2,8 @@ import express from "express";
 import { handlelike, getallLikedVideo } from "../controllers/like.js";
 
 const routes = express.Router();
-routes.get("/:userId", getallLikedVideo);
-routes.post("/:videoId", handlelike);
+
+routes.get("/user/:userId", getallLikedVideo);
+routes.post("/video/:videoId", handlelike);
+
 export default routes;
