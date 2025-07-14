10426788e3f4d17e5100d83594b2f21aa8790479
Dhanusha R <162958967+dhanusha013@users.noreply.github.com>
2025-07-14 19:57:22 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ const allowedOrigins = [
 app.use(cors({
   origin: function (origin, callback) {
     if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
+      callback(null, origin);
     } else {
       callback(new Error("Not allowed by CORS"));
     }
